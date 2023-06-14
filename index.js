c9a7e2b05b0d6ce187f1cc98158db2eab6168e84
mcolosimo86 <136358733+mcolosimo86@users.noreply.github.com>
2023-06-14 17:23:50 +0200

    Add Forth and Fifth

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ document.getElementById("demo").innerHTML = "Primo<br>";
 document.getElementById("demo").innerHTML += "Secondo<br>";
 document.getElementById("demo").innerHTML += "<b>Terzo</b><br>";
 document.getElementById("demo").innerHTML += "Quarto<br>";
+document.getElementById("demo").innerHTML += "Quinto<br>";
