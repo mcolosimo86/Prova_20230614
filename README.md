3746c3761e8f7bcbe552e9818fff986e6431af86
mcolosimo86 <136358733+mcolosimo86@users.noreply.github.com>
2023-06-14 17:18:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Feedback sul DOM
+
+L'esempio illustra come seguire l'esecuzione del programma inserendo il feedback nel DOM e con notifiche visibili sull'interfaccia grafica
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sswinnerhtml)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+document.getElementById("demo").innerHTML = "Primo<br>";
+document.getElementById("demo").innerHTML += "Secondo<br>";
+document.getElementById("demo").innerHTML += "<b>Terzo</b><br>";
+document.getElementById("demo").innerHTML += "Quarto<br>";
