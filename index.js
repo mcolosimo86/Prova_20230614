a3bd10d3e0524ce609f98a526a7201f7dd52a10f
mcolosimo86 <136358733+mcolosimo86@users.noreply.github.com>
2023-06-14 17:35:11 +0200

    RemoveTagbr

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ document.getElementById("demo").innerHTML = "Primo<br>";
 document.getElementById("demo").innerHTML += "Secondo<br>";
 document.getElementById("demo").innerHTML += "<b>Terzo</b><br>";
 document.getElementById("demo").innerHTML += "Quarto<br>";
-document.getElementById("demo").innerHTML += "Quinto<br>";
+document.getElementById("demo").innerHTML += "Quinto";
